a8b157ada69eb41e3a2893dc305856b1915fc2e1
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-02-13 16:42:08 +0100

    [ECS] switch from bean to java-data

--- src/clj/aliyuncs/ecs/instance.clj
+++ src/clj/aliyuncs/ecs/instance.clj
@@ -10,10 +10,11 @@
             CreateInstanceResponse
             DeleteInstanceRequest
             DeleteInstanceResponse])
-    (:require [aliyuncs.core :as acs]))
+    (:require [aliyuncs.core :as acs])
+    (:use [clojure.java.data]))
 
 (defn describe-instances
   [client]
   (let [desc-request (DescribeInstancesRequest.)
         ^DescribeInstancesResponse desc-response (acs/get-response client desc-request)]
-        (bean desc-response)))
+        (from-java desc-response)))
