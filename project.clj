a5ad670b8c78e5304fc7c7bae629b9c9b30e106f
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-09-12 15:21:58 +0200

    [CLOJARS]

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-aliyun "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject luhhujbb/clj-aliyun "0.1.0"
+  :description "Basic clojure lib to interact with aliyun ECS API"
+  :url "https://github.com/luhhujbb/clj-aliyun"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
