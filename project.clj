64668882c4830e1c84ffd22e2de547af7c71c9df
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2018-10-15 09:33:49 +0200

    [release] v0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luhhujbb/clj-aliyun "0.2.3"
+(defproject luhhujbb/clj-aliyun "0.2.4"
   :description "Basic clojure lib to interact with aliyun ECS API"
   :url "https://github.com/luhhujbb/clj-aliyun"
   :license {:name "Eclipse Public License"
